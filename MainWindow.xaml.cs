5bdb9a31f1f7838d275aead5e4979251338c0c9f
Brandon Unkrur <bunkrur@logicworks.net>
2021-04-27 20:13:56 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloGlobe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowState = WindowState.Maximized;
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+
+        private void uxPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            enableSubmitButton();
+        }
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enableSubmitButton();
+        }
+        private void enableSubmitButton()
+        {
+            if ((uxName.Text != "") && (uxPassword.Text != ""))
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = false;
+            }
+        }
+    }
+}
