f94d1697ffcc94d196c520da76959155807761d3
Brandon Unkrur <b.unkrur@gmail.com>
2021-05-19 16:20:43 -0700

    homework4:init

---
+++ Homework4/Homework4/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using Homework4;
+
+namespace Homework4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ZipCode zipCode = new ZipCode();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddressBox.DataContext = zipCode;
+            
+        }
+        private void ZipCodeChanged(object sender, TextChangedEventArgs e)
+        {
+            Submit.IsEnabled = zipCode.ValidZipCode;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Homework4/Homework4/Models/ZipCode.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace Homework4
+{
+    class ZipCode : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string zip = string.Empty;
+        private string zipError;
+
+        public string Zip
+        {
+            get { return zip; }
+            set { if (zip != value)
+                {
+                    zip = value;
+                    OnPropertyChanged("ZipCode");
+                }
+                    
+                        }
+        }
+        public string Error
+        {
+            get
+            {
+                return ZipCodeError;
+            }
+        }
+
+        public string ZipCodeError
+        {
+            get { return zipError; }
+            private set { zipError = value; }
+        }
+
+        readonly string usZipRegEx = @"^\d{5}(?:[-\s]\d{4})?$";
+        readonly string caZipRegEx = @"^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$";
+
+        private bool IsUSOrCanadianZipCode(string zipCode)
+        {
+            var validZipCode = true;
+            if ((!Regex.Match(zipCode, usZipRegEx).Success) && (!Regex.Match(zipCode, caZipRegEx).Success))
+            {
+                validZipCode = false;
+            }
+            return validZipCode;
+        }
+
+
+
+        public bool ValidZipCode
+        {
+            get
+            {
+                return IsUSOrCanadianZipCode(Zip.ToUpper());
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string this[string columnName] => IsUSOrCanadianZipCode(Zip.ToUpper()).ToString();
+    }
+}
